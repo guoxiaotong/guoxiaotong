2a6ae6747ea9a87333e27c048677da6f34369d44
周贤池 <zhouxianchi@che3bao.com>
2016-03-31 13:28:02 +0800

    环信2.x聊天功能集成完毕

---
+++ guoxiaotong/guoxiaotong/ThirdPart/HuanXin/NickNameAndHeadImage.h
@@ -0,0 +1,13 @@
+//
+//  NickNameAndHeadImage.h
+//  guoxiaotong
+//
+//  Created by zxc on 16/3/30.
+//  Copyright © 2016年 StenpChou. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NickNameAndHeadImage : NSObject
+
+@end
---
+++ guoxiaotong/guoxiaotong/ThirdPart/HuanXin/NickNameAndHeadImage.m
@@ -0,0 +1,13 @@
+//
+//  NickNameAndHeadImage.m
+//  guoxiaotong
+//
+//  Created by zxc on 16/3/30.
+//  Copyright © 2016年 StenpChou. All rights reserved.
+//
+
+#import "NickNameAndHeadImage.h"
+
+@implementation NickNameAndHeadImage
+
+@end
